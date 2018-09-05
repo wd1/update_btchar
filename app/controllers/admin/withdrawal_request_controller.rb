8ed88a34a343a3e3c1ed4b8ca80017ea7ec4fb38
wd1ror1 <wd1ror1@yahoo.com>
2018-09-05 12:22:55 -0400

    Admin Withdraw add

--- app/controllers/admin/withdrawal_request_controller.rb
+++ app/controllers/admin/withdrawal_request_controller.rb
@@ -1,17 +1,27 @@
 module Admin
-  class WithdrawalRequestController < BaseController
+  class WithdrawalRequestController < ::Admin::Withdraws::BaseController
     skip_load_and_authorize_resource
 
+    load_resource :class => '::Withdraws::Satoshi'
+
     def index
-      # @daemon_statuses = Global.daemon_statuses
-      # @currencies_summary = Currency.all.map(&:summary)
-      # @register_count = Member.count
-      @all = Withdraw.all
+      start_at = DateTime.now.ago(60 * 60 * 24)
+      @one_alls = @alls.with_aasm_state(:accepted).order("id DESC")
+      @all_alls = @alls.without_aasm_state(:accepted).where('created_at > ?', start_at).order("id DESC")
     end
 
     def show
       @all = Withdraw.all
     end
 
+    def update
+      @all.process!
+      redirect_to :back, notice: t('.notice')
+    end
+
+    def destroy
+      @all.reject!
+      redirect_to :back, notice: t('.notice')
+    end
   end
 end
